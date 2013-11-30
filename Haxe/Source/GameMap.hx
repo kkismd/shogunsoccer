9d49438eec0baa066dfe4efbcaa8f10d6d1f86f9
SHIMADA Keiki <shimada.cake@gmail.com>
2013-11-30 20:11:59 +0900

    座標型を定義

--- Haxe/Source/GameMap.hx
+++ Haxe/Source/GameMap.hx
@@ -4,39 +4,41 @@ import flash.geom.Point;
 import GameSequence.MapObject;
 class GameMap {
     private var map:Array<MapObject>;
-    private var origin:Point;
 
-    public function new(origin:Point)
+    public function new()
     {
-        this.origin = origin;
-
         map = new Array<MapObject>();
         for (i in 0...81)
         {
-            map.push(MapObject.None);
+            map[i] = MapObject.None;
         }
     }
 
-    public function put(me:MapObject, pos:Point):Void
+    public function put(me:MapObject, pos:LogicalCoordinate):Void
     {
         var address = pointToAddress(pos);
         map[address] = me;
     }
 
-    public function at(pos:Point):MapObject
+    public function at(pos:LogicalCoordinate):MapObject
     {
         var address = pointToAddress(pos);
         return map[address];
     }
 
-    private function pointToAddress(point:Point):Int
+    private function pointToAddress(point:LogicalCoordinate):Int
     {
         return Std.int(point.x + point.y * 9);
     }
-
-    private function getUnitPos(pos:Point):Point {
-        var posX = (pos.x * 30 + 15) * 1.5 + origin.x;
-        var posY = (pos.y * 32 + 15) * 1.5 + origin.y;
-        return new Point(posX, posY);
-    }
 }
+
+typedef LogicalCoordinate = {
+    x: Int,
+    y: Int,
+}
+
+typedef GraphicalCoordinate = {
+    x: Float,
+    y: Float,
+}
+
--- Haxe/Source/GameSequence.hx
+++ Haxe/Source/GameSequence.hx
@@ -1,13 +1,14 @@
 package;
 
-import motion.Actuate;
-import flash.geom.Point;
-import flash.display.Sprite;
-import openfl.Assets;
-import flash.display.Bitmap;
 import BaseSequence.SequenceKind;
+import GameMap.LogicalCoordinate;
+import GameMap.GraphicalCoordinate;
+import flash.display.Bitmap;
 import flash.display.Stage;
+import flash.display.Sprite;
 import flash.ui.Keyboard;
+import motion.Actuate;
+import openfl.Assets;
 
 // 移動キー入力の方向
 enum Direction {
@@ -19,12 +20,14 @@ enum Direction {
 }
 
 // マップに置かれているオブジェクトの種類
-enum MapObject {
+enum MapObject
+{
     None;
     Player(id : String);
 }
 
-class GameSequence implements BaseSequence {
+class GameSequence implements BaseSequence
+{
     private var main:Main;
     private var stage:Stage;
     private var tokin:Bitmap;
@@ -34,33 +37,36 @@ class GameSequence implements BaseSequence {
     private var direction:Direction;
     private var ballMoveX = 3.0;
     private var ballMoveY = 4.0;
-    private var myPos:Point;
-    private var map:Array<MapObject>;
+    private var myPos:LogicalCoordinate;
+    private var map:GameMap;
 
-    public function new(main:Main, stage:Stage) {
+    public function new(main:Main, stage:Stage)
+    {
         this.main = main;
         this.stage = stage;
     }
 
     // 初期化
-    public function start():Void {
+    public function start():Void
+    {
         initMyUnit();
         initBoard();
         initBall();
         initUnit();
     }
 
-    private function initMyUnit():Void {
+    private function initMyUnit():Void
+    {
         direction = Direction.Stop;
-        myPos = new Point(0, 0);
+        myPos = {x:0, y:0};
     }
 
-    private function initBoard():Void {
-        map = new Array<MapObject>();
-        for (i in 0...81) {
-            map.push(MapObject.None);
-        }
-        map[0] = MapObject.Player('me');
+    private function initBoard():Void
+    {
+        map = new GameMap();
+        var me = MapObject.Player("me");
+        var pos:LogicalCoordinate = {x: 0, y: 0};
+        map.put(me, pos);
 
         board = loadBitmap('assets/japanese-chess-bds.jpg');
         board.x = 50;
@@ -70,7 +76,8 @@ class GameSequence implements BaseSequence {
         main.addChild(board);
     }
 
-    private function initBall():Void {
+    private function initBall():Void
+    {
         ballBase = new Sprite();
         ballBase.x = 60;
         ballBase.y = 50;
@@ -82,7 +89,8 @@ class GameSequence implements BaseSequence {
         ballBase.addChild(ball);
     }
 
-    private function initUnit():Void {
+    private function initUnit():Void
+    {
         tokin = loadBitmap('assets/sgs18.png');
         tokin.x = (stage.stageWidth - tokin.width) / 2;
         tokin.y = (stage.stageHeight - tokin.height) / 2;
@@ -92,16 +100,19 @@ class GameSequence implements BaseSequence {
     }
 
     // メインループ
-    public function update():SequenceKind {
+    public function update():SequenceKind
+    {
         ballBase.rotation += 2;
         moveBall();
         inputDirection();
-        if (main.count() % 4 == 0) {
+        if (main.count() % 4 == 0)
+        {
             moveTokin();
         }
 
         // キーボードの Q で終了
-        if (main.isKeyPress(Keyboard.Q)) {
+        if (main.isKeyPress(Keyboard.Q))
+        {
             clearObjects();
             return SequenceKind.Title;
         }
@@ -109,104 +120,131 @@ class GameSequence implements BaseSequence {
         return SequenceKind.Stay;
     }
 
-    private function moveTokin():Void {
-        var oldPos:Point = myPos;
+    private function moveTokin():Void
+    {
+        var oldPos : LogicalCoordinate = {x: myPos.x, y: myPos.y};
 
-        if (direction == Direction.Up &&myPos.y > 0) {
+        if (direction == Direction.Up &&myPos.y > 0)
+        {
             myPos.y -= 1;
-        } else if (direction == Direction.Down && myPos.y < 8) {
+        }
+        else if (direction == Direction.Down && myPos.y < 8)
+        {
             myPos.y += 1;
-        } else if (direction == Direction.Left && myPos.x > 0) {
+        }
+        else if (direction == Direction.Left && myPos.x > 0)
+        {
             myPos.x -= 1;
-        } else if (direction == Direction.Right && myPos.x < 8) {
+        }
+        else if (direction == Direction.Right && myPos.x < 8)
+        {
             myPos.x += 1;
         }
         // 移動していたらマップを書き換える
         if (myPos != oldPos) {
-            map[pointToMapAddress(oldPos)] = MapObject.None;
-            map[pointToMapAddress(myPos)] = MapObject.Player("me");
+            map.put(MapObject.None, oldPos);
+            map.put(MapObject.Player("me"), myPos);
         }
         animateTokin(myPos);
     }
 
-    private function animateTokin(pos:Point):Void {
+    private function animateTokin(pos:LogicalCoordinate):Void
+    {
         var newPos = getUnitPos(myPos);
         Actuate.tween(tokin, 1, {x: newPos.x, y: newPos.y});
     }
 
-    private function moveBall():Void {
+    private function moveBall():Void
+    {
         // ボールが右向きではみ出していないなら
-        if (ballMoveX > 0 && ballBase.x < board.x + board.width - ball.width / 2) {
+        if (ballMoveX > 0 && ballBase.x < board.x + board.width - ball.width / 2)
+        {
             // 右に動く
             ballBase.x += ballMoveX;
         }
         // ボールが右向きではみだしているなら
-        else if (ballMoveX > 0 && ballBase.x >= board.x + board.width - ball.width/ 2) {
+        else if (ballMoveX > 0 && ballBase.x >= board.x + board.width - ball.width/ 2)
+        {
             // 反転させる
             ballMoveX = -ballMoveX;
         }
         // ボールが左向きではみ出していない
-        else if (ballMoveX < 0 && ballBase.x - ball.width / 2 > board.x) {
+        else if (ballMoveX < 0 && ballBase.x - ball.width / 2 > board.x)
+        {
             // 左に動く
             ballBase.x += ballMoveX;
         }
         // ボールが左向きではみ出している
-        else if (ballMoveX < 0 && ballBase.x - ball.width / 2 <= board.x) {
+        else if (ballMoveX < 0 && ballBase.x - ball.width / 2 <= board.x)
+        {
             // 反転させる
             ballMoveX = -ballMoveX;
         }
 
         // ボールが下向きではみだしていない
-        if (ballMoveY > 0 && ballBase.y < board.y + board.height - ball.height / 2) {
+        if (ballMoveY > 0 && ballBase.y < board.y + board.height - ball.height / 2)
+        {
             // 下に動く
             ballBase.y += ballMoveY;
         }
         // 下向きではみ出している
-        else if (ballMoveY > 0 && ballBase.y >= board.y + board.height - ball.height / 2) {
+        else if (ballMoveY > 0 && ballBase.y >= board.y + board.height - ball.height / 2)
+        {
             ballMoveY = -ballMoveY;
         }
         // 上向きではみ出していない
-        else if (ballMoveY < 0 && ballBase.y - ball.height / 2 > board.y) {
+        else if (ballMoveY < 0 && ballBase.y - ball.height / 2 > board.y)
+        {
             ballBase.y += ballMoveY;
         }
         // 上向きではみ出している
-        else if (ballMoveY < 0 && ballBase.y - ball.height / 2 <= board.y) {
+        else if (ballMoveY < 0 && ballBase.y - ball.height / 2 <= board.y)
+        {
             // 反転させる
             ballMoveY = -ballMoveY;
         }
     }
 
-    function inputDirection() {
-        if (main.isKeyPress(Keyboard.UP)) {
+    function inputDirection():Void
+    {
+        if (main.isKeyPress(Keyboard.UP))
+        {
             direction = Direction.Up;
-        } else if (main.isKeyPress(Keyboard.DOWN)) {
+        }
+        else if (main.isKeyPress(Keyboard.DOWN))
+        {
             direction = Direction.Down;
-        } else if (main.isKeyPress(Keyboard.LEFT)) {
+        }
+        else if (main.isKeyPress(Keyboard.LEFT))
+        {
             direction = Direction.Left;
-        } else if (main.isKeyPress(Keyboard.RIGHT)) {
+        }
+        else if (main.isKeyPress(Keyboard.RIGHT))
+        {
             direction = Direction.Right;
-        } else {
+        }
+        else
+        {
             direction = Direction.Stop;
         }
     }
 
-    private function clearObjects():Void {
+    private function clearObjects():Void
+    {
         main.removeChild(tokin);
         main.removeChild(board);
         main.removeChild(ballBase);
     }
 
-    private function loadBitmap(path:String):Bitmap {
+    private function loadBitmap(path:String):Bitmap
+    {
         return new Bitmap(Assets.getBitmapData(path));
     }
 
-    private function getUnitPos(pos:Point):Point {
+    private function getUnitPos(pos:LogicalCoordinate):GraphicalCoordinate
+    {
         var posX = (pos.x * 30 + 15) * 1.5 + board.x;
         var posY = (pos.y * 32 + 15) * 1.5 + board.y;
-        return new Point(posX, posY);
-    }
-
-    private function pointToMapAddress(point:Point):Int {
-        return Std.int(point.x + point.y * 9);
+        return {x: posX, y: posY};
     }
 }
